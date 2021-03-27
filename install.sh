1ffc47da92887b733ce0e15dc908b0155ee7241e
Toshimitsu Watanabe <kz86nu@gmail.com>
2021-03-27 22:56:20 +0900

    a

--- install.sh
+++ install.sh
@@ -1,21 +1,32 @@
 #!/bin/bash
 WHO=$(whoami)
+echo " ---------------"
+echo "| Hello ${WHO}! |"
+echo " ---------------"
 
-echo "Hello ${WHO}!"
+echo "----------------------------------------------"
+echo "Run apt/yum update..."
+echo "----------------------------------------------"
 apt-get update -y || yum update -y
 rm /var/lib/dpkg/lock
 rm /var/lib/dpkg/lock-frontend
 rm /var/cache/apt/archives/lock
 
 if [ $WHO != "root" ]; then
+  echo "----------------------------------------------"
   echo "Before installing brew,"
   echo "Install liblary to install brew requirements"
+  echo "----------------------------------------------"
   which brew || apt-get install -y build-essential curl file git || yum groupinstall -y 'Development Tools'; yum install -y curl file git; yum install -y libxcrypt-compat
+  echo "----------------------------------------------"
   echo "Now, start installing brew"
+  echo "----------------------------------------------"
   which brew || /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 fi
 
+# locales-all should be installed first. See https://qiita.com/suzuki-navi/items/b5f066db181092543854
 apps=(
+  locales-all
   cat
   curl
   fzf
@@ -27,6 +38,7 @@ apps=(
   vim
   zip
   zsh
+  tmux
 )
 
 TEST_BREW=$(which brew)
@@ -34,17 +46,24 @@ TEST_APT=$(which apt-get)
 TEST_YUM=$(which yum)
 
 if [ $TEST_BREW ] && [ $WHO != "root" ]; then
+  echo "----------------------------------------------"
   echo "Brew was installed!!"
+  echo "----------------------------------------------"
   eval '$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)'
 
+  echo "----------------------------------------------"
   echo "Run brew doctor..."
-  which brew && brew doctor
+  echo "----------------------------------------------"
+  brew doctor
 
+  echo "----------------------------------------------"
   echo "Run brew update..."
-  which brew && brew update
+  echo "----------------------------------------------"
+  brew update
 
-  echo "Brew is setup"
-
+  echo "----------------------------------------------"
+  echo "install apps"
+  echo "----------------------------------------------"
   for app in "${apps[@]}"; do
     which $app || brew install -y $app || brew upgrade -y $app
   done
